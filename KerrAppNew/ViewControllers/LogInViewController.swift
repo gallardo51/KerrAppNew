7f35555466cda804ee7d0a4199d728f203b887c9
Alex Sobolev <31108@mail.ru>
2024-01-07 17:44:49 +0300

    Setup TabBarController and NavigationControllers

---
+++ KerrAppNew/ViewControllers/LogInViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LogInViewController.swift
+//  KerrAppNew
+//
+//  Created by Александр Соболев on 07.01.2024.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
