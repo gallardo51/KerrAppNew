97db64b425c261cd9baad69ea3813384fcecadef
Alex Sobolev <31108@mail.ru>
2024-01-07 18:52:33 +0300

    SetupVC

---
+++ KerrAppNew/ViewControllers/CompanyInfoViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CompanyInfoViewController.swift
+//  KerrAppNew
+//
+//  Created by Александр Соболев on 07.01.2024.
+//
+
+import UIKit
+
+class CompanyInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+  
+
+}
