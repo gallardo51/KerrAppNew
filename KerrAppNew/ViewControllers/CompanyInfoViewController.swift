d3481412e004492b438c25ae0bb0c91b874be1b4
Alex Sobolev <31108@mail.ru>
2024-01-21 16:58:10 +0300

    Setup stack of companyVC

--- KerrAppNew/ViewControllers/CompanyInfoViewController.swift
+++ KerrAppNew/ViewControllers/CompanyInfoViewController.swift
@@ -8,9 +8,27 @@
 import UIKit
 
 class CompanyInfoViewController: UIViewController {
-
+    
+    private let infoCompany = CompanyInformation.getCompanyInfo()
+    
+    
+    @IBOutlet weak var titleAboutCompany: UILabel!
+    @IBOutlet weak var aboutCompany: UILabel!
+    @IBOutlet weak var titleHistory: UILabel!
+    @IBOutlet weak var companyHistory: UILabel!
+    @IBOutlet weak var titleHeadquarters: UILabel!
+    @IBOutlet weak var headquarters: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        titleAboutCompany.text = infoCompany.titleAboutCompany
+        aboutCompany.text = infoCompany.companyInfo
+        titleHistory.text = infoCompany.titleHistory
+        companyHistory.text = infoCompany.history
+        titleHeadquarters.text = infoCompany.titleHeadquarters
+        headquarters.text = infoCompany.headquarters
 
     }
     
