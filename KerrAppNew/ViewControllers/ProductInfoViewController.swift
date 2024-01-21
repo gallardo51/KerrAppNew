6199e11565f93220c4582dee9bacc9e4fb488fea
Alex Sobolev <31108@mail.ru>
2024-01-21 16:14:27 +0300

    Setup view controllers

---
+++ KerrAppNew/ViewControllers/ProductInfoViewController.swift
@@ -0,0 +1,11 @@
+//
+//  ProductInfoViewController.swift
+//  KerrAppNew
+//
+//  Created by Александр Соболев on 21.01.2024.
+//
+
+import UIKit
+
+class ProductInfoViewController: UIViewController {
+}
