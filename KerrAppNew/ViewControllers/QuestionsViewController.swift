d2e3538371fd15d51ffe21df4b7590042778eede
Alex Sobolev <31108@mail.ru>
2024-02-10 16:57:45 +0300

    Setup logic of QuestionsVC

--- KerrAppNew/ViewControllers/QuestionsViewController.swift
+++ KerrAppNew/ViewControllers/QuestionsViewController.swift
@@ -8,22 +8,64 @@
 import UIKit
 
 class QuestionsViewController: UIViewController {
-
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var questionProgressView: UIProgressView!
+    
+    @IBOutlet var answerChosenButtons: [UIButton]!
+    
+    private let questions = Question.getQuestions()
+    private var answersChosen: [Answer] = []
+    private var currentAnswers: [Answer] {
+        questions[questionIndex].answers
+    }
+    private var questionIndex = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        updateUI()
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        guard let buttonIndex = answerChosenButtons.firstIndex(of: sender) else { return }
+        let currentAnswer = currentAnswers[buttonIndex]
+        answersChosen.append(currentAnswer)
+        
+        nextQuestion()
     }
-    */
+    
+}
 
+extension QuestionsViewController {
+    private func updateUI() {
+        
+        let currentQuestion = questions[questionIndex]
+        
+        questionLabel.text = currentQuestion.title
+        
+        let totalProgress = Float(questionIndex) / Float(questions.count)
+        
+        questionProgressView.setProgress(totalProgress, animated: true)
+        
+        title = "Вопрос № \(questionIndex + 1) из \(questions.count)"
+        
+        showCurrentAnswers(with: currentAnswers)
+    }
+    
+    private func showCurrentAnswers(with answers: [Answer]) {
+        for (button, answer) in zip(answerChosenButtons, answers) {
+            button.setTitle(answer.title, for: .normal)
+        }
+    }
+    private func nextQuestion() {
+        questionIndex += 1
+        
+        if questionIndex < questions.count {
+            updateUI()
+            return
+        }
+        
+        performSegue(withIdentifier: "showResult", sender: nil)
+    }
 }
